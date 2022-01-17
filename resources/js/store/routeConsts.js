ef53b6b8c6029334a820f1f2a0ef5909bf4ac949
Vladislav <dev4rweb@gmail.com>
2022-01-17 13:46:53 +0200

    Changing rootpath

--- resources/js/store/routeConsts.js
+++ resources/js/store/routeConsts.js
@@ -4,4 +4,4 @@ export const LOCAL_HOST = 'http://127.0.0.1:8001/api/'
 const stagingHost = 'http://staging-admin.newstarmlm.biz/api/'
 // const testProducts = 'http://staging-admin.newstarmlm.biz/api/customer-order/products'
 // const testComments = 'http://staging-admin.newstarmlm.biz/api/customer-order/collector'
-export const WORK_HOST = LOCAL_HOST
+export const WORK_HOST = stagingHost
