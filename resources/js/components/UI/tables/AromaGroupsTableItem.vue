24b8ec4089d4cdac5241538a112fb6e97d48aac5
Vladislav <dev4rweb@gmail.com>
2022-03-15 13:17:01 +0200

    Adding data to AromaTable from basket

--- resources/js/components/UI/tables/AromaGroupsTableItem.vue
+++ resources/js/components/UI/tables/AromaGroupsTableItem.vue
@@ -9,8 +9,11 @@
                 type="number"
                 class="form-control"
                 min="0"
-                :class="{disabled: isDisabledTwo}"
-                :disabled="isDisabledTwo"
+                name="2мл"
+                v-model="amountTwo"
+                :class="{disabled: !isDisabledTwo}"
+                :disabled="!isDisabledTwo"
+                @blur="addToBasket"
             >
         </td>
         <td>
@@ -18,8 +21,11 @@
                 type="number"
                 class="form-control"
                 min="0"
-                :class="{disabled: isDisabledThree}"
-                :disabled="isDisabledThree"
+                name="3мл"
+                v-model="amountThree"
+                :class="{disabled: !isDisabledThree}"
+                :disabled="!isDisabledThree"
+                @blur="addToBasket"
             >
         </td>
         <td>
@@ -27,8 +33,11 @@
                 type="number"
                 class="form-control"
                 min="0"
-                :class="{disabled: isDisabledSeven}"
-                :disabled="isDisabledSeven"
+                name="7мл"
+                v-model="amountSeven"
+                :class="{disabled: !isDisabledSeven}"
+                :disabled="!isDisabledSeven"
+                @blur="addToBasket"
             >
         </td>
         <td>
@@ -36,35 +45,132 @@
                 type="number"
                 class="form-control"
                 min="0"
-                :class="{disabled: isDisabledTwelve}"
-                :disabled="isDisabledTwelve"
+                name="12мл"
+                v-model="amountTwelve"
+                :class="{disabled: !isDisabledTwelve}"
+                :disabled="!isDisabledTwelve"
+                @blur="addToBasket"
             >
         </td>
         <td>
             <input
                 type="number"
                 class="form-control"
-                :class="{disabled: isDisabledFifty}"
-                :disabled="isDisabledFifty"
+                min="0"
+                name="50мл"
+                v-model="amountFifty"
+                :class="{disabled: !isDisabledFifty}"
+                :disabled="!isDisabledFifty"
+                @blur="addToBasket"
             >
         </td>
     </tr>
 </template>
 
 <script>
-import {mapGetters} from 'vuex'
+import {mapGetters, mapMutations, mapActions} from 'vuex'
 
 export default {
     name: "AromaGroupsTableItem",
     props: ['product'],
-    methods: {},
+    data() {
+        return {
+            amountTwo: 0,
+            amountThree: 0,
+            amountSeven: 0,
+            amountTwelve: 0,
+            amountFifty: 0,
+        }
+    },
+    methods: {
+        ...mapMutations(['setToastError']),
+        ...mapActions(['addToBasketAction', 'removeFromBasketAction']),
+        addToBasket(e) {
+            console.log('addToBasket', e.target.value)
+            if (e.target.value < 0) {
+                this.setToastError('Некорректное значение');
+                e.target.value = 0;
+            }
+            if (e.target.value >= 0) {
+                // console.log('addToBasket target name', e.target.name)
+                const curProduct = this.getCategoryProducts
+                    .find(i => i.name.includes(this.product.code)
+                        &&
+                        i.name.includes(e.target.name))
+                console.log('addToBasket curProduct', curProduct)
+                let curAmount = null
+                switch (e.target.name) {
+                    case '2мл':
+                        curAmount = this.amountTwo
+                        break
+                    case '3мл':
+                        curAmount = this.amountThree
+                        break
+                    case '7мл':
+                        curAmount = this.amountSeven
+                        break
+                    case '12мл':
+                        curAmount = this.amountTwelve
+                        break
+                    case '50мл':
+                        curAmount = this.amountFifty
+                        break
+                }
+                if (curProduct) {
+                    const orderObj = {
+                        prodId: curProduct.vendor_code,
+                        amount: curAmount,
+                        product: curProduct,
+                        created_at: Date.now(),
+                    };
+                    console.log('addToBasket orderObj', orderObj)
+                    if (e.target.value > 0) this.addToBasketAction(orderObj)
+                    if (e.target.value == 0) this.removeFromBasketAction(orderObj)
+                }
+            }
+        },
+        checkProdGroup() {
+            if (this.getLSOrder) {
+                const groupProds = this.getLSOrder.filter(i => this.getVendorExp(i.prodId) === this.product.code)
+                if (groupProds.length) {
+                    groupProds.forEach(groupProd => {
+                        console.log('checkAmountTwo lsOrder&product', this.getLSOrder, this.product)
+                        console.log('checkAmountTwo groupProd', groupProd)
+                        const lastNumberProdId = groupProd.prodId.charAt(3) + groupProd.prodId.charAt(4)
+                        console.log('checkAmountTwo lastNumberProdId', lastNumberProdId)
+                        if (lastNumberProdId === '02') this.amountTwo = groupProd.amount
+                        if (lastNumberProdId === '03') this.amountThree = groupProd.amount
+                        if (lastNumberProdId === '07') this.amountSeven = groupProd.amount
+                        if (lastNumberProdId === '12') this.amountTwelve = groupProd.amount
+                        if (lastNumberProdId === '50') this.amountFifty = groupProd.amount
+                    });
+                }
+            }
+        },
+        getVendorExp(prodId) {
+            const firstNumber = prodId.charAt(0)
+            const aromaNumber = prodId.charAt(1) + prodId.charAt(2)
+            switch (firstNumber) {
+                case '1':
+                    return `C-${aromaNumber}`
+                case '2':
+                    return `D-${aromaNumber}`
+                case '3':
+                    return `K-${aromaNumber}`
+                default:
+                    return `M-${aromaNumber}`
+            }
+        },
+    },
     computed: {
-        ...mapGetters(['getCategoryProducts']),
+        ...mapGetters(['getCategoryProducts', 'getLSOrder']),
+
         isDisabledTwo() {
             const curProd = this.getCategoryProducts
                 .find(i => i.name.includes(this.product.code)
                     &&
                     i.name.includes('2мл'))
+            // console.log('isDisabledTwo', curProd, !!curProd)
             return !!curProd;
         },
         isDisabledThree() {
@@ -95,6 +201,9 @@ export default {
                     i.name.includes('50мл'))
             return !!curProd;
         },
+    },
+    mounted() {
+        this.checkProdGroup()
     }
 }
 </script>
